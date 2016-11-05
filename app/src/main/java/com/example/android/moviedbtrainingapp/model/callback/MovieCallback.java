60a09147da32f1cffdfc17130601d6932462866f
Milten <Milten198@gmail.com>
2016-11-05 10:41:14 +0100

    Endless list with issue

--- app/src/main/java/com/example/android/moviedbtrainingapp/model/callback/MovieCallback.java
+++ app/src/main/java/com/example/android/moviedbtrainingapp/model/callback/MovieCallback.java
@@ -5,9 +5,10 @@ import com.example.android.moviedbtrainingapp.model.MoviesResponse;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Path;
 import retrofit2.http.Query;
 
 public interface MovieCallback {
     @GET("movie/top_rated")
-    Call<MoviesResponse> getTopRatedMovies(@Query("api_key") String apiKey);
+    Call<MoviesResponse> getTopRatedMovies(@Query("api_key") String apiKey, @Query("page") int page);
 }
--- app/src/main/java/com/example/android/moviedbtrainingapp/ui/MainActivity.java
+++ app/src/main/java/com/example/android/moviedbtrainingapp/ui/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 
 import com.example.android.moviedbtrainingapp.R;
 import com.example.android.moviedbtrainingapp.controller.MovieRestManager;
@@ -14,6 +15,7 @@ import com.example.android.moviedbtrainingapp.model.adapter.MovieAdapter;
 import com.example.android.moviedbtrainingapp.model.callback.MovieCallback;
 import com.example.android.moviedbtrainingapp.model.utils.Constants;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import retrofit2.Call;
@@ -24,23 +26,33 @@ public class MainActivity extends AppCompatActivity implements MovieAdapter.Movi
 
     RecyclerView movieRecycle;
     MovieAdapter adapter;
-    List<Movie> movies;
+    private boolean loading = true;
+    int pastVisiblesItems, visibleItemCount, totalItemCount;
+    int page = 1;
+    List<Movie> allMovies;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        allMovies = new ArrayList<>();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
         downloadMoviesData();
     }
 
     public void downloadMoviesData() {
         MovieCallback callback = MovieRestManager.getClient().create(MovieCallback.class);
-        Call<MoviesResponse> call = callback.getTopRatedMovies(Constants.API_KEY.API_KEY);
+        Call<MoviesResponse> call = callback.getTopRatedMovies(Constants.API_KEY.API_KEY, page);
         call.enqueue(new Callback<MoviesResponse>() {
             @Override
             public void onResponse(Call<MoviesResponse> call, Response<MoviesResponse> response) {
                 List<Movie> moviesList = response.body().getResults();
-                setAdapter(moviesList);
+                allMovies.addAll(moviesList);
+                setAdapter(allMovies);
             }
 
             @Override
@@ -52,10 +64,33 @@ public class MainActivity extends AppCompatActivity implements MovieAdapter.Movi
 
     public void setAdapter(List<Movie> movies) {
         movieRecycle = (RecyclerView) findViewById(R.id.movieList);
+        final LinearLayoutManager movieLinearManager = new LinearLayoutManager(this);
+        movieRecycle.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+                if(dy > 0)
+                {
+                    visibleItemCount = movieLinearManager.getChildCount();
+                    totalItemCount = movieLinearManager.getItemCount();
+                    pastVisiblesItems = movieLinearManager.findFirstVisibleItemPosition();
+
+                    if (loading)
+                    {
+                        if ( (visibleItemCount + pastVisiblesItems) >= totalItemCount)
+                        {
+                            page++;
+                            downloadMoviesData();
+                            adapter.notifyDataSetChanged();
+                        }
+                    }
+                }
+            }
+        });
         adapter = new MovieAdapter(this, this);
         adapter.getMovies(movies);
         movieRecycle.setAdapter(adapter);
-        movieRecycle.setLayoutManager(new LinearLayoutManager(this));
+        movieRecycle.setLayoutManager(movieLinearManager);
+        movieRecycle.setHasFixedSize(true);
     }
 
     @Override
