69884b103d92be2f4d092fc097273c8e8e9088a1
Milten <Milten198@gmail.com>
2016-12-01 17:37:49 +0100

    Stetho

---
+++ app/src/main/java/com/example/android/moviedbtrainingapp/controller/MyClientBuilder.java
@@ -0,0 +1,24 @@
+package com.example.android.moviedbtrainingapp.controller;
+
+
+import com.facebook.stetho.okhttp3.StethoInterceptor;
+
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.OkHttpClient;
+
+public class MyClientBuilder {
+
+    protected static OkHttpClient configureClient() {
+        OkHttpClient client = null;
+        OkHttpClient.Builder builder = new OkHttpClient.Builder();
+        builder.readTimeout(180, TimeUnit.SECONDS);
+        builder.addNetworkInterceptor(new StethoInterceptor());
+        client = builder.build();
+        return client;
+    }
+
+    public static OkHttpClient createClient() {
+        return configureClient();
+    }
+}
