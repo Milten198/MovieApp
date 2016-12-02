86a8aca3e496dcf02485006bf77bb6700f94bba5
Piotr Sulej <piotr.sulej@performgroup.com>
2016-12-02 15:16:44 +0100

    Empty branch

--- app/src/main/java/com/example/android/moviedbtrainingapp/model/utils/NumberRounder.java
+++
@@ -1,8 +0,0 @@
-package com.example.android.moviedbtrainingapp.model.utils;
-
-
-public class NumberRounder {
-    public static String twoDecimalNumbers(double numberToRound) {
-        return String.format("%.2f", numberToRound);
-    }
-}
